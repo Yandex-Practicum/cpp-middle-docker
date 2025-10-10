1f967a46c4a64e1c4efaffe7957cad89033e73cf
Ivan Losev <ivan.s.nn97@gmail.com>
2025-10-10 15:47:30 +0200

    Улучшил контейнер, добавил тесты и подключил все используемые библиотеки

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,30 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 from conan.tools.files import copy, rmdir
+from conan.tools.layout import basic_layout
 import os
 
-class CryptoGuardConan(ConanFile):
-    name = "books_db"
+class CppMiddleCourseConanFile(ConanFile):
+    name = "cpp_middle"
     version = "0.0.1"
     settings = "os", "compiler", "build_type", "arch"
     
     def requirements(self):
-        self.tool_requires("cmake/3.31.6")
+        self.tool_requires("cmake/3.30.0")
 
+        #
+        # We are including all the dependencies, which will be used during the cpp-middle course
+        #
         self.requires("benchmark/1.9.1")
+        self.requires("gtest/1.13.0")
         self.requires("abseil/20250127.0")
-        self.requires("boost/1.87.0")
-        # self.requires("openssl/3.4.1")
-        # self.requires("sqlite3/3.49.1")
-        pass
+        self.requires("boost/1.83.0")
+        self.requires("openssl/3.1.1")
+        self.requires("sqlite3/3.49.1")
+        self.requires("range-v3/0.12.0")
     
     def layout(self):
-        self.folders.source = "."
-        self.folders.build = "build"
-        self.folders.generators = "build/generators"
+        basic_layout(self, src_folder=".", build_folder="build")
     
     def generate(self):
         deps = CMakeDeps(self)
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <flat_set>
-#include <print>
-
-int main() {
-    if (__cplusplus == 202302L) std::println("Hello, C++23!");
-
-    std::flat_set<int> fs;
-    fs.emplace(1);
-}
